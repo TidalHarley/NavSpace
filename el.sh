5d295975c47e834ed51b3090ff6bd9fc325f660c
TidalHarley <Harley_yang@hotmail.com>
2025-09-30 14:36:27 +0800

    Update el.sh

--- el.sh
+++ el.sh
@@ -4,12 +4,12 @@ cleanup() {
     echo "All processes terminated."
 }
 
-# 捕获 EXIT 和 SIGINT 信号
 trap cleanup EXIT SIGINT
 #sleep 5h 
 for i in {0,1,2,3,4,5,6,7}
 do
     CUDA_VISIBLE_DEVICES=$((i)) python gpt_eval.py --model_id $((i))  &
 done
+
 
 wait
